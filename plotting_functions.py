53849c14435bf94f9cd3961733bf4741a0785ee5
Sarah Pristash <srp137@uw.edu>
2021-03-15 19:17:04 -0700

    added timeslider

--- plotting_functions.py
+++ plotting_functions.py
@@ -2,15 +2,13 @@ import fiona.crs
 import folium
 import geopandas as gpd
 import geojson
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import requests
 import urllib.request as request
-
-def plot_zip_traffic_data(year):
-    '''function that takes a user input for a year and plots Seattle traffic data by zipcode.'''
-
-    url_list = ['https://opendata.arcgis.com/datasets/7015d5d46a284f94ac05c2ea4358bcd7_0.geojson',
+url_list = ['https://opendata.arcgis.com/datasets/7015d5d46a284f94ac05c2ea4358bcd7_0.geojson',
             'https://opendata.arcgis.com/datasets/5fc63b2a48474100b560a7d98b5097d7_1.geojson',
             'https://opendata.arcgis.com/datasets/27af9a2485c5442bb061fa7e881d7022_2.geojson',
             'https://opendata.arcgis.com/datasets/4f62515558174f53979b3be0335004d3_3.geojson',
@@ -22,9 +20,8 @@ def plot_zip_traffic_data(year):
             'https://opendata.arcgis.com/datasets/170b764c52f34c9497720c0463f3b58b_9.geojson',
             'https://opendata.arcgis.com/datasets/2c37babc94d64bbb938a9b520bc5538c_10.geojson',
             'https://opendata.arcgis.com/datasets/a35aa9249110472ba2c69cc574eff984_11.geojson']
-    
-
-    def get_gdf(year):
+            
+def get_gdf(year):
         '''Enter the desired year to download the traffic flow count
         data for that year. Example: enter '7' for the year 2007.
         '''
@@ -63,38 +60,41 @@ def plot_zip_traffic_data(year):
         gdf_year = gdf_year[gdf_year.YEAR.notnull()]
         return gdf_year 
 
-    def get_census_bounds():
-        ''' Downloads boundaries of census tracts for the city of Seattle. Data comes from Seattle's open GIS data.'''
-        # First, we download the data from Seattle's open GIS. 
-        url = 'https://opendata.arcgis.com/datasets/de58dc3e1efc49b782ab357e044ea20c_9.geojson'
-        census_bounds = gpd.read_file(url)
-        # We select only the relevant columns.
-        census_columns = ['NAME10', 'SHAPE_Area', 'geometry']
-        census_bounds_cleaned = census_bounds.loc[:,census_columns]
-        # We change the cenus tract name to an integer for manipulation later.
-        census_bounds_cleaned['NAME10'] = census_bounds_cleaned['NAME10'].astype(float)
-        return census_bounds_cleaned
+def get_census_bounds():
+    ''' Downloads boundaries of census tracts for the city of Seattle. Data comes from Seattle's open GIS data.'''
+    # First, we download the data from Seattle's open GIS. 
+    url = 'https://opendata.arcgis.com/datasets/de58dc3e1efc49b782ab357e044ea20c_9.geojson'
+    census_bounds = gpd.read_file(url)
+    # We select only the relevant columns.
+    census_columns = ['NAME10', 'SHAPE_Area', 'geometry']
+    census_bounds_cleaned = census_bounds.loc[:,census_columns]
+    # We change the cenus tract name to an integer for manipulation later.
+    census_bounds_cleaned['NAME10'] = census_bounds_cleaned['NAME10'].astype(float)
+    return census_bounds_cleaned
 
 
-    def get_zipcode_bounds():
-        ''' Downloads boundaries of zipcodes for the city of Seattle. Data comes from Seattle's open GIS data.'''
-        # First, we download the data from Seattle's open GIS.
-        zipcodes_url = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
-        zipcodes = gpd.read_file(zipcodes_url)
-        # We select only the relevant columns and change the Zipcode to an integer for manipulation later.
-        zipcodes_columns = ['ZIPCODE', 'SHAPE_Area', 'geometry']
-        zipcodes_cleaned = zipcodes.loc[:,zipcodes_columns]
-        zipcodes_cleaned['ZIPCODE'] = zipcodes_cleaned['ZIPCODE'].astype(int)
-        zipcodes_cleaned.head()
-        # The zipcode dataset includes more than just the area of Seattle, so we join it 
-        # with the census bounds to get out only Seattle data. 
-        census_bounds_cleaned = get_census_bounds()
-        zips = gpd.sjoin(zipcodes_cleaned, census_bounds_cleaned, op='intersects')
-        zips_columns = ['ZIPCODE', 'NAME10', 'SHAPE_Area_left', 'geometry']
-        zips = zips[zips_columns]
-        # Finally, we dissolve the data by Zipcode to ensure one area for each zipcode.
-        zips = zips.dissolve(by='ZIPCODE')
-        return zips
+def get_zipcode_bounds():
+    ''' Downloads boundaries of zipcodes for the city of Seattle. Data comes from Seattle's open GIS data.'''
+    # First, we download the data from Seattle's open GIS.
+    zipcodes_url = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
+    zipcodes = gpd.read_file(zipcodes_url)
+    # We select only the relevant columns and change the Zipcode to an integer for manipulation later.
+    zipcodes_columns = ['ZIPCODE', 'SHAPE_Area', 'geometry']
+    zipcodes_cleaned = zipcodes.loc[:,zipcodes_columns]
+    zipcodes_cleaned['ZIPCODE'] = zipcodes_cleaned['ZIPCODE'].astype(int)
+    zipcodes_cleaned.head()
+    # The zipcode dataset includes more than just the area of Seattle, so we join it 
+    # with the census bounds to get out only Seattle data. 
+    census_bounds_cleaned = get_census_bounds()
+    zips = gpd.sjoin(zipcodes_cleaned, census_bounds_cleaned, op='intersects')
+    zips_columns = ['ZIPCODE', 'NAME10', 'SHAPE_Area_left', 'geometry']
+    zips = zips[zips_columns]
+    # Finally, we dissolve the data by Zipcode to ensure one area for each zipcode.
+    zips = zips.dissolve(by='ZIPCODE')
+    return zips
+
+def plot_zip_traffic_data(year):
+    '''function that takes a user input for a year and plots Seattle traffic data by zipcode.'''
     
     # We need to download the zipcode data and the traffic data for the desired year. 
     zips = get_zipcode_bounds()
@@ -151,5 +151,71 @@ def plot_zip_traffic_data(year):
     
     return m
     
+def plot_traffic_data_over_time():
+    '''Function that plots all Seattle traffic data by zipcode over the time period 2007-2018.'''
+    
+    print('Function takes a moment to run. Please wait.')
+    
+    # Aggregate all traffic data by year
+    year_list = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+    agg_year_data = gpd.GeoDataFrame()
+    for year in year_list:
+        agg_year_data = agg_year_data.append(get_gdf(year))
+    
+    # Download zip bounds
+    zip_bounds = get_zipcode_bounds()
+    # Set aggregated year data to zip bound coordinate system
+    agg_year_data.crs = zip_bounds.crs
+    # Spatial join the aggregated traffic data with the zip codes to get zipcode for each street points
+    city_by_zip = gpd.sjoin(zip_bounds, agg_year_data, op='intersects')
+    city_by_zip.reset_index(inplace = True)
+    # Select only the necessary columns
+    city_by_zip = city_by_zip[['GEOBASID','ZIPCODE', 'YEAR', 'AAWDT', 'geometry']]
+    # Dissolve data by zipcode and year to aggregate data within geographic area
+    # and keep year info.
+    zips_years = city_by_zip.dissolve(by=['YEAR','ZIPCODE'], aggfunc=sum)
+    zips_years.reset_index(inplace=True)
+    
+    from folium.plugins import TimeSliderChoropleth
+    # Convert time data from just year to year-month-day format
+    zips_years['ModifiedDateTime'] = pd.Series(pd.to_numeric(zips_years['YEAR'], errors='coerce'), dtype='int64')
+    zips_years.ModifiedDateTime.fillna(0)
+    zips_years['ModifiedDateTime'] = zips_years['ModifiedDateTime']*1e4+101
+    zips_years['ModifiedDateTime'] = pd.to_datetime(zips_years['ModifiedDateTime'].astype('int64').astype('str'))
+    # Convert traffic data from strings to numbers
+    zips_years['AAWDT'] = zips_years['AAWDT'].astype(int)
+    # Create bins for choropleth scale
+    bins=np.linspace(min(zips_years['AAWDT']),max(zips_years['AAWDT']),11)
+    # Create color column for AAWDT data based on RdYlBu_r hex keys
+    zips_years['color'] = zips_years['AAWDT']=pd.cut(zips_years['AAWDT'],bins,labels=['#313695',\
+        '4575b4','#74add1','#abd9e9','#e0f3f8','#ffffbf','#fee090',\
+        '#fdae61','#f46d43','a50026'],include_lowest=True)
+    # Select relevant columns
+    zips_years = zips_years[['ModifiedDateTime', 'ZIPCODE', 'AAWDT', 'color', 'geometry']]
+    # Convert time to ms format needed for TimeSliderChoropleth
+    zips_years['ModifiedDateTime']=(zips_years['ModifiedDateTime'].astype(int)// 10**9).astype('U10')
+    # Make zipcodes a str for the map
+    zips_years['ZIPCODE'] = zips_years['ZIPCODE'].astype(str)
+    # Create a style dictionary for the map
+    traffic_dict={}
+    for i in zips_years['ZIPCODE'].unique():
+        traffic_dict[i]={}
+        for j in zips_years[zips_years['ZIPCODE']==i].set_index(['ZIPCODE']).values:   
+            traffic_dict[i][j[0]]={'color':j[1],'opacity':0.8}
+    
+    m2 = folium.Map([47.65, -122.3], tiles='cartodbpositron', zoom_start=10)
+
+    g = TimeSliderChoropleth(
+        zips_years.set_index('ZIPCODE').to_json(),
+        styledict=traffic_dict
+    ).add_to(m2)
+
+    return m2
+    
+    
+    
     
         
+
+
+    
