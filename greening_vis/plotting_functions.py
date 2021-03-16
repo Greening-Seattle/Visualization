9831b450fed4e463940fa72c29ab17a66f80d0c9
Sarah Pristash <srp137@uw.edu>
2021-03-15 22:50:59 -0700

    fixed some flake8 issues

--- greening_vis/plotting_functions.py
+++ greening_vis/plotting_functions.py
@@ -1,64 +1,64 @@
-import fiona.crs
 import folium
 import geopandas as gpd
-import geojson
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import requests
-import urllib.request as request
-url_list = ['https://opendata.arcgis.com/datasets/7015d5d46a284f94ac05c2ea4358bcd7_0.geojson',
-            'https://opendata.arcgis.com/datasets/5fc63b2a48474100b560a7d98b5097d7_1.geojson',
-            'https://opendata.arcgis.com/datasets/27af9a2485c5442bb061fa7e881d7022_2.geojson',
-            'https://opendata.arcgis.com/datasets/4f62515558174f53979b3be0335004d3_3.geojson',
-            'https://opendata.arcgis.com/datasets/29f801d03c9b4b608bca6a8e497278c3_4.geojson',
-            'https://opendata.arcgis.com/datasets/a0019dd0d6464747a88921f5e103d509_5.geojson',
-            'https://opendata.arcgis.com/datasets/40bcfbc4054549ebba8b5777bbdd40ff_6.geojson',
-            'https://opendata.arcgis.com/datasets/16cedd233d914118a275c6510115d466_7.geojson',
-            'https://opendata.arcgis.com/datasets/902fd604ecf54adf8579894508cacc68_8.geojson',
-            'https://opendata.arcgis.com/datasets/170b764c52f34c9497720c0463f3b58b_9.geojson',
-            'https://opendata.arcgis.com/datasets/2c37babc94d64bbb938a9b520bc5538c_10.geojson',
-            'https://opendata.arcgis.com/datasets/a35aa9249110472ba2c69cc574eff984_11.geojson']
+url_list = [
+ 'https://opendata.arcgis.com/datasets/7015d5d46a284f94ac05c2ea4358bcd7_0.geojson',
+ 'https://opendata.arcgis.com/datasets/5fc63b2a48474100b560a7d98b5097d7_1.geojson',
+ 'https://opendata.arcgis.com/datasets/27af9a2485c5442bb061fa7e881d7022_2.geojson',
+ 'https://opendata.arcgis.com/datasets/4f62515558174f53979b3be0335004d3_3.geojson',
+ 'https://opendata.arcgis.com/datasets/29f801d03c9b4b608bca6a8e497278c3_4.geojson',
+ 'https://opendata.arcgis.com/datasets/a0019dd0d6464747a88921f5e103d509_5.geojson',
+ 'https://opendata.arcgis.com/datasets/40bcfbc4054549ebba8b5777bbdd40ff_6.geojson',
+ 'https://opendata.arcgis.com/datasets/16cedd233d914118a275c6510115d466_7.geojson',
+ 'https://opendata.arcgis.com/datasets/902fd604ecf54adf8579894508cacc68_8.geojson',
+ 'https://opendata.arcgis.com/datasets/170b764c52f34c9497720c0463f3b58b_9.geojson',
+ 'https://opendata.arcgis.com/datasets/2c37babc94d64bbb938a9b520bc5538c_10.geojson',
+ 'https://opendata.arcgis.com/datasets/a35aa9249110472ba2c69cc574eff984_11.geojson']
             
 def get_gdf(year):
         '''Enter the desired year to download the traffic flow count
         data for that year. Example: enter '7' for the year 2007.
         '''
-        # Pulls the data from Seattle's open GIS.
-        num = year-7
-        gdf_year = gpd.read_file(url_list[num])
-        # There are some inconsistencies in the names of columns across years in this data, 
-        # so these conditional statements make everything the same.
-        if year == 11:
-            gdf_year = gdf_year.rename(columns={"YEAR_" : 'YEAR'})
-            gdf_year = gdf_year[gdf_year.STNAME != '16TH AVE S']
-        if year == 12:
-            gdf_year = gdf_year.rename(columns={'STDY_YEAR' : 'YEAR'})
-        if year == 15 or year == 16:
-            gdf_year = gdf_year.rename(columns={"COUNTAAWDT" : 'AAWDT', "FLOWSEGID" : "GEOBASID", 'FIRST_STNAME_ORD' : 'STNAME'})
-            gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
-            if year == 15:
-                year_list = [2015]*len(gdf_year)
-                gdf_year['YEAR'] = year_list
-            elif year == 16:
-                year_list = [2016]*len(gdf_year)
-                gdf_year['YEAR'] = year_list
-        elif year == 17 or year == 18:
-            gdf_year = gdf_year.rename(columns={"AWDT" : 'AAWDT', "FLOWSEGID" : "GEOBASID", 'STNAME_ORD' : 'STNAME'})
-            gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
-            if year == 17:
-                year_list = [2017]*len(gdf_year)
-                gdf_year['YEAR'] = year_list
-            elif year == 18:
-                year_list = [2018]*len(gdf_year)
-                gdf_year['YEAR'] = year_list
-        # This cleans the output to contain only relevant columns.
-        gdf_year = gdf_year[[ 'YEAR', 'AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
-        # This removes any null values from the dataset. 
-        gdf_year = gdf_year[gdf_year.YEAR != 0]
-        gdf_year = gdf_year[gdf_year.YEAR.notnull()]
-        return gdf_year 
+        
+        
+    # Pulls the data from Seattle's open GIS.
+    num = year-7
+    gdf_year = gpd.read_file(url_list[num])
+    # There are some inconsistencies in the names of columns across years in this data, 
+    # so these conditional statements make everything the same.
+    if year == 11:
+        gdf_year = gdf_year.rename(columns={"YEAR_":'YEAR'})
+        gdf_year = gdf_year[gdf_year.STNAME != '16TH AVE S']
+    if year == 12:
+        gdf_year = gdf_year.rename(columns={'STDY_YEAR':'YEAR'})
+    if year == 15 or year == 16:
+        gdf_year = gdf_year.rename(columns={"COUNTAAWDT":'AAWDT',\ 
+        "FLOWSEGID":"GEOBASID", 'FIRST_STNAME_ORD':'STNAME'})
+        gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME',\
+        'SHAPE_Length', 'geometry']]
+        if year == 15:
+            year_list = [2015]*len(gdf_year)
+            gdf_year['YEAR'] = year_list
+        elif year == 16:
+            year_list = [2016]*len(gdf_year)
+            gdf_year['YEAR'] = year_list
+    elif year == 17 or year == 18:
+        gdf_year = gdf_year.rename(columns={"AWDT" : 'AAWDT', "FLOWSEGID" : "GEOBASID", 'STNAME_ORD' : 'STNAME'})
+        gdf_year = gdf_year[['AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
+        if year == 17:
+            year_list = [2017]*len(gdf_year)
+            gdf_year['YEAR'] = year_list
+        elif year == 18:
+            year_list = [2018]*len(gdf_year)
+            gdf_year['YEAR'] = year_list
+    # This cleans the output to contain only relevant columns.
+    gdf_year = gdf_year[[ 'YEAR', 'AAWDT', 'GEOBASID', 'STNAME', 'SHAPE_Length', 'geometry']]
+    # This removes any null values from the dataset. 
+    gdf_year = gdf_year[gdf_year.YEAR != 0]
+    gdf_year = gdf_year[gdf_year.YEAR.notnull()]
+    return gdf_year 
 
 def get_census_bounds():
     ''' Downloads boundaries of census tracts for the city of Seattle. Data comes from Seattle's open GIS data.'''
